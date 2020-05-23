49e0f13d4389c5386773caa27586f1f9e2fd5e14
aatris39 <60397253+aatris39@users.noreply.github.com>
2020-05-22 21:43:54 -0400

    Update challenge-3.py

--- challenge-3.py
+++ challenge-3.py
@@ -1,7 +1,10 @@
 print("Pick a number.")
 snum = input()
 num = int(snum)
-
+print("How many multiples?")
+smt = input()
+mt = int(smt)
 # These statements print different things...
-print(snum * 2)
-print(num * 2)
+
+for num in range(mt):
+   print(num * 2) 
